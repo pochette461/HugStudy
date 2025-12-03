2af04a0cba87d1b8d5275a1fa7158cd33981be60
pochette461 <nonnnnne.non@gmail.com>
2025-12-03 14:40:31 +0900

    2-6

---
+++ 2-6/Task2_6.java
@@ -0,0 +1,14 @@
+public class Task2_6{
+    public static void main(String[]args){
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        //double型を宣言して、「hogeDouble」という変数名をつけて、0.8で初期化する。
+        double hogeDouble = 0.8;
+        hogeInt = 100;
+        hogeString ="良くできました！";
+        System.out.println(hogeInt);
+        //変数 hogeStringの値を表示
+        System.out.println(hogeString);
+        System.out.println(hogeDouble);
+    }
+}
